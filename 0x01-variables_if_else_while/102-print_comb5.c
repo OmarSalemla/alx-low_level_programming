52b121b05e9d59fcb079d84c5b0f7f25d3c5f191
OmarSalemla <dromarsalem04@gmail.com>
2023-09-09 06:42:06 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,29 +21,11 @@ int main(void)
 		{
 			if (i != j)
 			{
-				if (i < 10)
-				{
-					putchar('0');
-					putchar(i + '0');
-				}
-				else
-				{
-					putchar(i / 10 + '0');
-					putchar(i % 10 + '0');
-				}
-
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-
-				if (j < 10)
-				{
-					putchar('0');
-					putchar(j + '0');
-				}
-				else
-				{
-					putchar(j / 10 + '0');
-					putchar(j % 10 + '0');
-				}
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 
 				if (i != 98 || j != 99)
 				{
