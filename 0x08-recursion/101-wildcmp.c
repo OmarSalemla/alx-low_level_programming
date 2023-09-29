9d77dee3007de9380b83c4e1bea522cac6c3b60d
OmarSalemla <dromarsalem04@gmail.com>
2023-09-28 17:45:11 -0700

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,6 +9,7 @@
  */
 char *move_past_star(char *s2)
 {
+
 	if (*s2 == '*')
 		return (move_past_star(s2 + 1));
 	else
@@ -28,12 +29,12 @@ int inception(char *s1, char *s2)
 
 	if (*s1 == 0)
 		return (0);
+
 	if (*s1 == *s2)
 		ret += wildcmp(s1 + 1, s2 + 1);
 	ret += inception(s1 + 1, s2);
-		return (ret);
+	return (ret);
 }
-
 /**
  * wildcmp - compares two strings textographically
  * @s1: the first string
@@ -47,23 +48,28 @@ int wildcmp(char *s1, char *s2)
 
 	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
+
 	if (*s1 == *s2)
 	{
 		if (!*s1)
 			return (1);
 		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-				}
-				if (!*s1 || !*s2)
-				return (0);
-				if (*s2 == '*')
-				{
-				s2 = move_past_star(s2);
-				if (!s2)
-				return (1);
-				if (*s1 == *s2)
-				ret += wildcmp(s1 + 1, s2 + 1);
-				ret += inception(s1, s2);
-				return (!!ret);
-				}
-				return (0);
-				}
+	}
+	if (!*s1 || !*s2)
+		return (0);
+
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+
+		if (!s2)
+			return (1);
+
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+
+		return (!!ret);
+	}
+	return (0);
+}
